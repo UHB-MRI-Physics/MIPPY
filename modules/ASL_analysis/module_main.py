18e8a46dbc32b7180e87c45155970a767eeb09e7
wesolek <drwesolek@icloud.com>
2016-12-15 10:03:01 +0000

    updates?

--- modules/ASL_analysis/module_main.py
+++ modules/ASL_analysis/module_main.py
@@ -42,25 +42,45 @@ def execute(master_window,dicomdir,images):
 	win = Toplevel(master = master_window)
 	
 	# Image Set Matrix Size
-	win.rows=images[-1].Rows
-	win.cols=images[-1].Columns
-        if 'PHILIPS' in images[0].Manufacturer.upper():
-                try:
-                        win.slcs=eval(images[-1][0x0028,0x0008].value)/eval(images[-1][0x2001,0x1081].value)
-                except:
-                        win.slcs=int(images[-1][0x2001,0x1018].value)
-                win.dyns=2*int(images[-1][0x2001,0x1081].value)
-        else:
-                win.slcs=images[-1].InstanceNumber/images[-1].AcquisitionNumber
-                win.dyns=images[-1].AcquisitionNumber
+        win.rows=images[-1].Rows
+        win.cols=images[-1].Columns
+            
+        slicepositions = []
+        try:
+                for im in images:
+                        slicepositions.append(str(im.PlanePositionSequence))
+        except:
+                for im in images:
+                    slicepositions.append(str(im.ImagePositionPatient))
+        slicepositions = np.array(slicepositions)
+        win.slcs = np.shape(np.unique(slicepositions))[0]
+        win.dyns = len(images)/win.slcs
 
-        print win.rows
-        print win.cols
-        print win.slcs
-        print win.dyns
-        print len(images)
+        try:
+                if (images[0].ImagePositionPatient==images[1].ImagePositionPatient):
+                    resort_needed = True
+                else:
+                    resort_needed = False
+        except AttributeError:
+                if (images[0].PlanePositionSequence==images[1].PlanePositionSequence):
+                    resort_needed = True
+                else:
+                    resort_needed = False
+            
+        if resort_needed:
+                sorted_images = []
+                for d in range(win.dyns):
+                        for s in range(win.slcs):
+                                sorted_images.append(images[s*win.dyns+d])
+                images=sorted_images
         
 	# Create canvases
+	n_canvases = []
+	for n in images
+                n_canvases.append(str(n.SeriesInstanceUID))
+        n_canvases = set(n_canvases)
+	print n_canvases
+	
 	win.imcanvases=Frame(win)
 	win.imcanvas_orig = MIPPYCanvas(win.imcanvases,bd=0 ,width=384,height=384,drawing_enabled=False)
 	win.imcanvas_out = MIPPYCanvas(win.imcanvases,bd=0 ,width=384,height=384,drawing_enabled=False)
@@ -124,17 +144,25 @@ def execute(master_window,dicomdir,images):
 	win.l_reject=Label(win.buttons,text="Pairs to removed from analysis")
 	win.reject_in=StringVar(win)
 	win.reject_in.set(0)
-	win.b_reject_in=Entry(win.buttons,textvariable=win.reject_in,width=20)
+	win.b_reject_in=Entry(win.buttons,textvariable=win.reject_in,width=5)
 
 	win.c_rev_in=IntVar(win)
 	win.c_rev_in.set(0)
 	win.c_rev=Checkbutton(win.buttons,text="Reverse Tag and Control",variable=win.c_rev_in)
 
+        win.l_T1bl=Label(win.buttons,text="T1 of Arterial Blood [ms]:")
+	win.T1bl_in=StringVar(win)
+	win.T1bl_in.set(1500)
+	win.b_T1bl_in=Entry(win.buttons,textvariable=win.T1bl_in,width=5)
+
+	win.l_T1tis=Label(win.buttons,text="T1 of Tissue [ms]:")
+	win.T1tis_in=StringVar(win)
+	win.T1tis_in.set(840)
+	win.b_T1tis_in=Entry(win.buttons,textvariable=win.T1tis_in,width=5)
+	
 	win.b_subtract = Button(win.buttons, text="Subtract", command=lambda:subtract(win))        
 	hide_rej(win)
 	win.b_average = Button(win.buttons, text="Average", command=lambda:average(win))
-	
-	win.b_T1corr = Button(win.buttons, text="T1 correction", command=lambda:T1corr(win))
 
 	# Window layout       
 	win.l_method.grid(row=0,column=0,sticky='nw')
@@ -144,9 +172,12 @@ def execute(master_window,dicomdir,images):
 	win.c_rev.grid(row=4,column=0,sticky='nw')
 	win.b_subtract.grid(row=5,column=0,sticky='nw')
 	win.b_average.grid(row=6,column=0,sticky='nw')
-	win.b_T1corr.grid(row=7,column=0,sticky='nw')
+	win.l_T1bl.grid(row=7,column=0,sticky='sw')
+	win.b_T1bl_in.grid(row=7,column=1,sticky='sw')
+	win.l_T1tis.grid(row=8,column=0,sticky='sw')
+	win.b_T1tis_in.grid(row=8,column=1,sticky='sw')
 
-	win.buttons.grid(row=0,column=1,sticky='new')
+	win.buttons.grid(row=0,column=1,sticky='news')
 	
 	# Resizing options
 	win.buttons.rowconfigure(0,weight=0)
@@ -162,7 +193,7 @@ def execute(master_window,dicomdir,images):
 	# Window layout
 	win.b_message.grid(row=0,column=0,sticky='news')
 
-	win.message_box.grid(row=1,column=0,sticky='wes')
+	win.message_box.grid(row=1,column=0,sticky='news')
 
 	# Resizing option
 	win.message_box.rowconfigure(0,weight=1)
@@ -171,14 +202,15 @@ def execute(master_window,dicomdir,images):
 	win.run_buttons=Frame(win)
 	print len(win.imcanvas_orig.images)
 	win.Im4D=np.array([a.px_float for a in win.imcanvas_orig.images]).reshape((win.dyns,win.slcs,win.rows,win.cols))
-#     win.b_run = Button(win.run_buttons, text="Run", command=lambda:realign(win))
+
+        win.b_perf = Button(win.buttons, text="Create Perfusion Maps", command=lambda:perf(win))
 	win.b_save = Button(win.run_buttons, text="Save", command=lambda:save(win,dicomdir,images))
 
 	# Window layout
-#        win.b_run.grid(row=0,column=0,sticky='new')
-	win.b_save.grid(row=1,column=0,sticky='new')
+        win.b_perf.grid(row=0,column=0,sticky='nw')
+	win.b_save.grid(row=1,column=0,sticky='nw')
 
-	win.run_buttons.grid(row=1,column=1,sticky="new")
+	win.run_buttons.grid(row=1,column=1,sticky="news")
 	
 	return
 	
@@ -187,12 +219,16 @@ def close_window(window):
 	active_frame.destroy()
 	return
 
-def status(win,txt):
-	win.b_message.config(state=NORMAL)
-	win.b_message.insert(END,txt)
-	win.b_message.config(state=DISABLED)
-	win.b_message.see(END)
-	win.update()
+def status(win,*txt):
+    print_str = ''
+    for arg in txt:
+        print_str = print_str+str(arg)
+    win.b_message.config(state=NORMAL)
+    win.b_message.insert(END,print_str+'\n')
+    win.b_message.bind('<1>', lambda event: win.b_message.focus_set())
+    win.b_message.config(state=DISABLED)
+    win.b_message.see(END)
+    win.update()
 
 def status_clear(win):
 	win.b_message.config(state=NORMAL)
@@ -256,11 +292,11 @@ def subtract(win):
 	win.images_processed=np.subtract(Im4Ds,Im4Dns)
 	if win.reject_in.get()!='0':
 		rejected=str2num(win.reject_in.get())
-		status(win,"rejected array: %s\n" %rejected)
+		status(win,"rejected array: %s" %rejected)
 		rejected=np.subtract(rejected,1)
 		win.images_processed=[element for n, element in enumerate(win.images_processed) if n not in rejected]
-	status(win,"DONE!!!\n")
-	status(win,"Number of Subtracted Pairs: %s\n" %np.size(win.images_processed,0))
+	status(win,"DONE!!!")
+	status(win,"Number of Subtracted Pairs: %s" %np.size(win.images_processed,0))
 	win.imcanvas_out.load_images(np.reshape(win.images_processed,(np.size(win.images_processed,0)*win.slcs,win.rows,win.cols)))
 	show_rej(win)
 	return
@@ -275,12 +311,12 @@ def average(win):
 	win.imcanvas_out.load_images(win.images_processed)
 	if win.toggle.find('av_')<0:
 		win.toggle+='av_'
-	status(win,'DONE!!!\n')
+	status(win,'DONE!!!')
 	return
         
 def save(win,dicomdir,images):
 	if hasattr(win,"images_processed"):
-		status(win,"Saving files... \n")
+		status(win,"Saving files...")
 		##        win.Im3D_align=np.reshape(win.Im4D_align,(win.dyns*win.slcs,win.rows,win.cols))
 		dicomdir_out=os.path.join(dicomdir,"_ASL")
 		if not os.path.exists(dicomdir_out):
@@ -334,9 +370,9 @@ def save(win,dicomdir,images):
 			file_out=os.path.join(dicomdir_out,"im_"+win.toggle+str(s+1).zfill(3)+".dcm")
 			images_out.save_as(file_out)
 		if s==0:
-			status(win,"Saved "+str(s+1)+" file in: "+dicomdir_out+"\n")
+                        status(win,'Saved ', s+1, 'file in: ',dicomdir_out)
 		else:
-			status(win,"Saved "+str(s+1)+" files in: "+dicomdir_out+"\n")
+                        status(win,'Saved ', s+1, 'files in: ',dicomdir_out)
 	else:
-		status(win,"There are no processed files to be saved...\n")
+		status(win,"There are no processed files to be saved... ")
 	return       
