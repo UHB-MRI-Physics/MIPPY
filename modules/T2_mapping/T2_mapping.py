e22a11c8757eb35f36606528f428dfebe0a2bc6f
wesolek <drwesolek@icloud.com>
2017-03-23 16:41:45 +0000

    T2 mapping ready for testing

--- modules/T2_mapping/T2_mapping.py
+++ modules/T2_mapping/T2_mapping.py
@@ -16,8 +16,8 @@ Functions here define functions to be fitted.  They must take "x" values as firs
 all additional variables to be fitted, returning "y" values (in the form of transformed/manipulated
 array of "x" values).
 """
-def inv_recovery(TI,M0,T1):
-    return abs(M0*(1-(2*np.exp(-TI/T1))))
+def T2_decay(TE,M0,T2):
+    return abs(M0*np.exp(-TE/T2))
 
 ###################################################
 
@@ -26,7 +26,7 @@ def inv_recovery(TI,M0,T1):
 # MAIN BODY OF PROGRAM
 ###################################################
 
-def T2_mapping(Im4D,TI,images,rev=False):
+def T2_mapping(Im4D,TE,images,threshold,GoF):
     outputdir = "maps"
 
     rows=np.size(Im4D,2)
@@ -34,13 +34,13 @@ def T2_mapping(Im4D,TI,images,rev=False):
     slcs=np.size(Im4D,1)
     dyns=np.size(Im4D,0)										
 
-    T1_map = np.zeros((slcs,rows,cols))
-    T1_var = np.zeros((slcs,rows,cols))
-    T1_R2 = np.zeros((slcs,rows,cols))
+    T2_map = np.zeros((slcs,rows,cols))
+    T2_var = np.zeros((slcs,rows,cols))
+    T2_R2 = np.zeros((slcs,rows,cols))
     M0_map = np.zeros((slcs,rows,cols))
     M0_var = np.zeros((slcs,rows,cols))
 
-    T1 = float(500)
+    T2 = float(50)
     #tolerance = 0.1
 								
     threshold = 0.05*np.amax(np.fabs(Im4D))
@@ -49,14 +49,14 @@ def T2_mapping(Im4D,TI,images,rev=False):
 
     for s in range(slcs):		                            							    
         print "Fitting slice "+str(s+1)+" of "+str(slcs)
-        print TI[s::dyns-1]
+        print TE[s::slcs]
         for y in range(rows):
             print "..."+str(int((float(y)/float(rows))*100))+"%"
             for x in range(cols):
                 if np.amax(np.fabs(Im4D[:,s,y,x]))<threshold:
-                    T1_map[s,y,x] = 0
-                    T1_var[s,y,x] = 0
-                    T1_R2[s,y,x] = 0
+                    T2_map[s,y,x] = 0
+                    T2_var[s,y,x] = 0
+                    T2_R2[s,y,x] = 0
                     M0_map[s,y,x]=0
                     M0_var[s,y,x]=0
 ##                    print "below threshold"
@@ -74,75 +74,55 @@ def T2_mapping(Im4D,TI,images,rev=False):
                     of each fit parameter.
                     """
 ##                    xdata = np.trim_zeros(TI[:,s,y,x],'b')
-                    xdata = TI[s::dyns-1]
+                    xdata = TE[s::slcs]
                     ydata = Im4D[0:len(xdata),s,y,x]
-                    T1 = -xdata[np.argmin(ydata)]/np.log(0.5)
+                    T2 = 50.
                     M0 = 1.2*np.amax(ydata)
-                    popt,pcov = curve_fit(inv_recovery,xdata,ydata,p0=[M0,T1])
+                    popt,pcov = curve_fit(T2_decay,xdata,ydata,p0=[M0,T2])
 
                     if not isinstance(pcov,float):                                                                                  # Checks to make sure covariance matrix is actually a matrix.  A failed fit gives a floating point "inf" value instead.
-                        T1_map[s,y,x] = popt[1]
+                        T2_map[s,y,x] = popt[1]
                         M0_map[s,y,x] = popt[0]
-                        T1_var[s,y,x] = pcov[1,1]
+                        T2_var[s,y,x] = pcov[1,1]
                         M0_var[s,y,x] = pcov[0,0]
                         
-                        fit = inv_recovery(xdata,popt[0],popt[1])
+                        fit = T2_decay(xdata,popt[0],popt[1])
                         
                         mean = np.mean(ydata)
                         SSres = np.sum((fit - ydata)**2)
                         SStot = np.sum((mean - ydata)**2)
-##                        T1_R2[s,y,x] = 1 - (SSres/SStot)
-                        T1_R2[s,y,x] = 100 - np.mean(abs(ydata-fit))/(np.max(ydata)-np.min(ydata))*100
-##                        print T1_R2[s,y,x]
-    #                    if R2[s,y,x]>0.99:
-    #                        T1 = T2 = T2star = popt[1]
-    #                        M0 = popt[0]
 
-    ##                    print popt[1],M0/np.amax(ydata),T1_R2[s,y,x]
+                        T2_R2[s,y,x] = 100 - np.mean(abs(ydata-fit))/(np.max(ydata)-np.min(ydata))*100
                         
                     else:                                                                                                                   # If fit failed and just "inf" exists instead of a covariance matrix, ignore the point, set the result and other parameters to zero
-                        T1_var[s,y,x] = 0
-                        T1_map[s,y,x] = 0
-                        T1_R2[s,y,x] = 0
+                        T2_var[s,y,x] = 0
+                        T2_map[s,y,x] = 0
+                        T2_R2[s,y,x] = 0
                         M0_map[s,y,x] = 0
                         M0_var[s,y,x] = 0
                         print "Fit failed, no optimised parameters"
                         
-                    if T1_R2[s,y,x]<50:
-                        T1_map[s,y,x] = 0
-                        T1_var[s,y,x] = 0
-                        T1_R2[s,y,x] = 0                                                                                                   # If calculated variance on result is larger than the result, also ignore and set to zero
+                    if T2_R2[s,y,x]<50:
+                        T2_map[s,y,x] = 0
+                        T2_var[s,y,x] = 0
+                        T2_R2[s,y,x] = 0                                                                                                   # If calculated variance on result is larger than the result, also ignore and set to zero
                         M0_map[s,y,x] = 0
                         M0_var[s,y,x] = 0
                         print "goodness of fit < 50%"
-##                        x_fit=np.array(range(0,int(np.round(np.amax(xdata),0))+10))
-##                        plt.errorbar(xdata,ydata,marker='o',linestyle='None',color='blue')
-##                        plt.errorbar(x_fit,inv_recovery(x_fit,popt[0],popt[1]),marker='None',linestyle='-',color='blue')
-##                        plt.show()
-                        
-                    # SSres = sum of square distances from fit
-                    # SStot = sum of square distances from mean
-                    # R2 = 1 - (SSres/SStot)
                     
                 except Exception, e:
                 
                     print str(e)
-                    T1_map[s,y,x]=0
-                    T1_var[s,y,x]=0
-                    T1_R2[s,y,x] = 0                                                                                                          # If fitting fails for some other reason, also set result to zero
+                    T2_map[s,y,x]=0
+                    T2_var[s,y,x]=0
+                    T2_R2[s,y,x] = 0                                                                                                          # If fitting fails for some other reason, also set result to zero
                     M0_map[s,y,x] = 0
                     M0_var[s,y,x] = 0
                     print "Unspecified exception"
 
 
-##                if x==32 and y==48 and s==4:
-##                    x_fit=np.array(range(0,int(np.round(np.amax(xdata),0))+10))
-##                    plt.errorbar(xdata,ydata,marker='o',linestyle='None',color='blue')
-##                    plt.errorbar(x_fit,inv_recovery(x_fit,popt[0],popt[1]),marker='None',linestyle='-',color='blue')
-##                    plt.show()
-
     #   clip R2 to a range of 0 to 1.  Negative numbers are meaningless.
 
-        T1_R2 = np.clip(T1_R2,0,100)
+        T2_R2 = np.clip(T2_R2,0,100)
 
-    return T1_map,T1_R2,T1_var,M0_map,M0_var
+    return T2_map,T2_R2,T2_var,M0_map,M0_var
--- modules/T2_mapping/module_main.py
+++ modules/T2_mapping/module_main.py
@@ -49,12 +49,14 @@ def execute(master_window,dicomdir,images):
         win.cols=images[-1].Columns
         
         slicepositions = []
+
         try:
                 for im in images:
                         slicepositions.append(str(im.PlanePositionSequence))
         except:
                 for im in images:
                         slicepositions.append(str(im.ImagePositionPatient))
+
         slicepositions = np.array(slicepositions)
         win.slcs = np.shape(np.unique(slicepositions))[0]
         win.dyns = len(images)/win.slcs
@@ -76,7 +78,7 @@ def execute(master_window,dicomdir,images):
                         for s in range(win.slcs):
                                 sorted_images.append(images[s*win.dyns+d])
                 images=sorted_images
-        
+
         # Create canvases
 	win.imcanvases=Frame(win)
 	win.imcanvas_orig = MIPPYCanvas(win.imcanvases,bd=0 ,width=384,height=384,drawing_enabled=True)
@@ -142,7 +144,7 @@ def execute(master_window,dicomdir,images):
         win.rb_TE = Radiobutton(win.buttons, text="Set Individual TEs (comma-separated) [ms]:", variable=win.rb_TEvar, value=2, command=lambda:show_TE(win))
         win.TE_in=StringVar(win)
         win.b_TE = Entry(win.buttons,textvariable=win.TE_in,width=30,state="disabled")
-        
+
         # Window layout
         
         win.rb_TEauto.grid(row=0,column=0,sticky='sw',rowspan=2,columnspan=4)
@@ -210,29 +212,29 @@ def status_clear(win):
         win.update()
 
 def hide_TE(win):
-        """Greys out Initial TA window"""
+        """Greys out TE window"""
         if win.rb_TEvar.get()==1:
                 win.b_TE.configure(state="disabled")
                 win.rb_TE.focus()
         return
 
 def show_TE(win):
-        """Activates the TA windows"""
+        """Activates the TE windows"""
         if win.rb_TEvar.get()==2:
                 win.b_TE.configure(state="normal")
                 win.b_TE.focus()
         return
 def T2_map(win,images):
-        """Creating maps - T1_mapping.py script"""
+        """Creating maps - T2_mapping.py script"""
         status_clear(win)
-        win.TIs=sort_TIs(win,images)
+        win.TEs=sort_TEs(win,images)
         
-        if win.TIs==None:
+        if win.TEs==None:
                 return
-        status(win,"Fitting T1 maps...\n")
+        status(win,"Fitting T2 maps...\n")
         start_time=time.time()
         win.maps=np.zeros((5,win.slcs,win.rows,win.cols))
-        win.maps=T1map(win.Im4D,win.TIs,images,rev=False)
+        win.maps=T2map(win.Im4D,win.TEs,images,threshold=0,GoF=50)
         
         run_time = time.time()-start_time
 
@@ -246,36 +248,28 @@ def T2_map(win,images):
                 
         return
 
-def sort_TIs(win,images):
-        """Sorting TIs for each individual image"""
-        status(win,"Sorting TIs for each individual image... \n")
-        TA=np.zeros(win.slcs-1)
-        TIs=np.zeros(win.dyns*win.slcs)
-        if win.rb_TAvar.get()==1:
-                for a in range(win.slcs-1):
-                        TA[a] = 1000*(float(images[a+1].AcquisitionTime)-float(images[a].AcquisitionTime))
-                TAav=int(np.mean(TA,0))
-                win.TA_in.set(TAav)
-                txt=("Estimated TA is %s [ms]\n" %(win.TA_in.get()))
-                status(win,txt)
-        if win.rb_TIvar.get()==1:
+def sort_TEs(win,images):
+        """Sorting TEs for each individual image"""
+        status(win,"Sorting TEs for each image... \n")
+        TEs=np.zeros(win.dyns*win.slcs)
+        if win.rb_TEvar.get()==1:
                 for d in range(win.dyns):
                         for s in range(win.slcs):
-                                TIs[int(win.slcs)*d+s]=int(win.TI_in.get())+d*int(win.TIinc_in.get())+s*int(win.TA_in.get())
-        elif win.rb_TIvar.get()==2:
-                TI=np.genfromtxt([win.TIs_in.get()],delimiter=",")
-                if np.size(TI,0)!=win.dyns:
-                        status(win,"Provided number of TIs does not match number of dynamics...\n")
+                                TEs[int(win.slcs)*d+s]=float(images[int(win.slcs)*d+s].EchoTime)
+        elif win.rb_TEvar.get()==2:
+                TE=np.genfromtxt([win.TEs_in.get()],delimiter=",")
+                if np.size(TE,0)!=win.dyns:
+                        status(win,"Provided number of TEs does not match number of dynamics...\n")
                         return None
                         
                 for d in range(win.dyns):
                         for s in range(win.slcs):
-                                TIs[int(win.slcs)*d+s]=TI[d]+s*win.TA_in
+                                TEs[int(win.slcs)*d+s]=s*win.TE_in.get()
 
-        status(win,"These are the follwing times for each image:\n")
-        status(win,TIs)
+        status(win,"These are the follwing echo times for each image:\n")
+        status(win,TEs)
         status(win,"\n")
-        return TIs
+        return TEs
 
 def save(win,dicomdir,images):
         status(win,"Saving maps in DICOM format... \n")
@@ -288,61 +282,61 @@ def save(win,dicomdir,images):
                 status(win,txt)
                 #   saving DICOMs
                 # T1 map
-                images_T1=images[len(images)-slcs+s-1]
-                images_T1.AcquisitionNumber=dyns+1
-                images_T1.InstanceNumber=dyns*slcs+s+1
-                T1_map=win.maps[0][s][:][:]
-                images_T1.PixelData = np.reshape(T1_map,(cols*rows)).astype(np.uint16)
-                images_T1.AcquisitionTime = str(float(images_T1.AcquisitionTime) + 0.1)
-                images_T1.SmallestImagePixelValue = min(images_T1.PixelData)
-                images_T1.LargestImagePixelValue = max(images_T1.PixelData)
-                images_T1.SOPInstanceUID = ''.join([images_T1.SOPInstanceUID,".1"])
-                images_T1.RescaleSlope = 1
-                images_T1.RescaleIntercept = np.min(T1_map)
+                images_T2=images[len(images)-slcs+s-1]
+                images_T2.AcquisitionNumber=dyns+1
+                images_T2.InstanceNumber=dyns*slcs+s+1
+                T2_map=win.maps[0][s][:][:]
+                images_T2.PixelData = np.reshape(T2_map,(cols*rows)).astype(np.uint16)
+                images_T2.AcquisitionTime = str(float(images_T2.AcquisitionTime) + 0.1)
+                images_T2.SmallestImagePixelValue = min(images_T2.PixelData)
+                images_T2.LargestImagePixelValue = max(images_T2.PixelData)
+                images_T2.SOPInstanceUID = ''.join([images_T2.SOPInstanceUID,".1"])
+                images_T2.RescaleSlope = 1
+                images_T2.RescaleIntercept = np.min(T2_map)
         ##        images_T1.SeriesDescription = "T1 result"
                 
-                file_out=os.path.join(dicomdir,"_Series_"+str(images_T1.SeriesNumber).zfill(3)+"_maps","T1_map_"+str(s+1).zfill(3)+".dcm")
+                file_out=os.path.join(dicomdir,"_Series_"+str(images_T2.SeriesNumber).zfill(3)+"_maps","T2_map_"+str(s+1).zfill(3)+".dcm")
                 try:
                         os.makedirs(os.path.split(file_out)[0])
                 except:
                         pass
 
-                images_T1.save_as(file_out)
+                images_T2.save_as(file_out)
                     
                 # T1_R^2
-                images_T1_R2=images[len(images)-slcs+s-1]
-                images_T1_R2.AcquisitionNumber=dyns+2
-                images_T1_R2.InstanceNumber=(dyns+1)*slcs+s+1
-                T1_R2=win.maps[1][s][:][:]
-                images_T1_R2.PixelData = np.uint16(T1_R2.reshape(cols*rows)) # Adjust the multiplication and then the RescaleSlope field
-                images_T1_R2.AcquisitionTime = str(float(images_T1_R2.AcquisitionTime) + 0.2)
-                images_T1_R2.SmallestImagePixelValue = min(images_T1_R2.PixelData)
-                images_T1_R2.LargestImagePixelValue = max(images_T1_R2.PixelData)
-                images_T1_R2.SOPInstanceUID = ''.join([images_T1_R2.SOPInstanceUID,".2"])
-                images_T1_R2.RescaleSlope = 1                                   # This should give you 100*R1, i.e. 99.55% => 99.55
-##                images_T1_R2.RescaleIntercept = np.min(T1_R2)
-                images_T1_R2.RescaleIntercept = 0
-        ##        images_T1_R2.SeriesDescription = "Fit R2 value"
+                images_T2_R2=images[len(images)-slcs+s-1]
+                images_T2_R2.AcquisitionNumber=dyns+2
+                images_T2_R2.InstanceNumber=(dyns+1)*slcs+s+1
+                T2_R2=win.maps[1][s][:][:]
+                images_T2_R2.PixelData = np.uint16(T2_R2.reshape(cols*rows)) # Adjust the multiplication and then the RescaleSlope field
+                images_T2_R2.AcquisitionTime = str(float(images_T2_R2.AcquisitionTime) + 0.2)
+                images_T2_R2.SmallestImagePixelValue = min(images_T2_R2.PixelData)
+                images_T2_R2.LargestImagePixelValue = max(images_T2_R2.PixelData)
+                images_T2_R2.SOPInstanceUID = ''.join([images_T2_R2.SOPInstanceUID,".2"])
+                images_T2_R2.RescaleSlope = 1                                   # This should give you 100*R1, i.e. 99.55% => 99.55
+##                images_T2_R2.RescaleIntercept = np.min(T2_R2)
+                images_T2_R2.RescaleIntercept = 0
+        ##        images_T2_R2.SeriesDescription = "Fit R2 value"
 
-                file_out=os.path.join(dicomdir,"_Series_"+str(images_T1_R2.SeriesNumber).zfill(3)+"_maps","T1_GoF_map_"+str(s+1).zfill(3)+".dcm")
-                images_T1_R2.save_as(file_out)
+                file_out=os.path.join(dicomdir,"_Series_"+str(images_T2_R2.SeriesNumber).zfill(3)+"_maps","T2_GoF_map_"+str(s+1).zfill(3)+".dcm")
+                images_T2_R2.save_as(file_out)
                     
                 # T1_Variance
-                images_T1_Var=images[len(images)-slcs+s-1]
-                images_T1_Var.AcquisitionNumber=dyns+3
-                images_T1_Var.InstanceNumber=(dyns+2)*slcs+s+1
-                T1_var=win.maps[2][s][:][:]
-                images_T1_Var.PixelData = np.uint16(10.*T1_var.reshape(cols*rows))
-                images_T1_Var.AcquisitionTime = str(float(images_T1_Var.AcquisitionTime) + 0.3)
-                images_T1_Var.SmallestImagePixelValue = min(images_T1_Var.PixelData)
-                images_T1_Var.LargestImagePixelValue = max(images_T1_Var.PixelData)
-                images_T1_Var.SOPInstanceUID = ''.join([images_T1_Var.SOPInstanceUID,".3"])
-                images_T1_Var.RescaleSlope = 1./10
-                images_T1_Var.RescaleIntercept = np.min(10*T1_var)
-        ##        images_T1_Var.SeriesDescription = "T1 variance"
+                images_T2_Var=images[len(images)-slcs+s-1]
+                images_T2_Var.AcquisitionNumber=dyns+3
+                images_T2_Var.InstanceNumber=(dyns+2)*slcs+s+1
+                T2_var=win.maps[2][s][:][:]
+                images_T2_Var.PixelData = np.uint16(10.*T2_var.reshape(cols*rows))
+                images_T2_Var.AcquisitionTime = str(float(images_T2_Var.AcquisitionTime) + 0.3)
+                images_T2_Var.SmallestImagePixelValue = min(images_T2_Var.PixelData)
+                images_T2_Var.LargestImagePixelValue = max(images_T2_Var.PixelData)
+                images_T2_Var.SOPInstanceUID = ''.join([images_T2_Var.SOPInstanceUID,".3"])
+                images_T2_Var.RescaleSlope = 1./10
+                images_T2_Var.RescaleIntercept = np.min(10*T2_var)
+        ##        images_T2_Var.SeriesDescription = "T2 variance"
 
-                file_out=os.path.join(dicomdir,"_Series_"+str(images_T1_Var.SeriesNumber).zfill(3)+"_maps","T1_Var_map_"+str(s+1).zfill(3)+".dcm")
-                images_T1_Var.save_as(file_out)
+                file_out=os.path.join(dicomdir,"_Series_"+str(images_T2_Var.SeriesNumber).zfill(3)+"_maps","T2_Var_map_"+str(s+1).zfill(3)+".dcm")
+                images_T2_Var.save_as(file_out)
                         
                 # M0 map
                 images_M0=images[len(images)-slcs+s-1]
