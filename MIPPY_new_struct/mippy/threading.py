39a404aca82fa3fa692f40d338e232c31e9e4f09
Rob Flintham <robert.flintham@uhb.nhs.uk>
2017-08-26 13:06:41 +0100

    Still getting memory errors

--- MIPPY_new_struct/mippy/threading.py
+++ MIPPY_new_struct/mippy/threading.py
@@ -8,13 +8,13 @@ def multithread(func,input,progressbar=None,threads=None):
 		threads=cpu_count()
 	with closing( Pool(threads) ) as pool:
 		result = pool.map_async(func,input,chunksize=1)
-		while not result.ready():
-			if not progressbar is None:
-				progress = (float(len(input))-float(result._number_left))/float(len(input))*100.
-				#~ print "PROGRESS", progress
-				progressbar(progress)
-			#~ print("num left: {}".format(result._number_left))
-			time.sleep(0.1)
+	while not result.ready():
+		if not progressbar is None:
+			progress = (float(len(input))-float(result._number_left))/float(len(input))*100.
+			#~ print "PROGRESS", progress
+			progressbar(progress)
+		#~ print("num left: {}".format(result._number_left))
+		time.sleep(0.1)
 	progressbar(0.)
 	pool.close()
 	pool.join()
