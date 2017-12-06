fe7c0aac6b81d32dd4517d9070134d864c099696
Rob Flintham <robert.flintham@uhb.nhs.uk>
2017-12-06 12:37:02 +0000

    1.0.0 Merged task72, including multi-slice ROI and colored ROI prescription

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 setup(	name='MIPPY',
-		version='0.24.0',
-		description='Meodular Image Processing in Python',
+		version='1.0.0',
+		description='Modular Image Processing in Python',
 		author='Robert Flintham',
 		author_email='robert.flintham@uhb.nhs.uk',
 		install_requires=['numpy','scipy','pydicom','pillow','nibabel','matplotlib'],
 		license='BSD-3-Clause',
 		classifiers=[
-			'Development Status :: 3 - Alpha',
+			'Development Status :: 1 - Alpha',
 			'Intended Audience :: Developers',
 			'Programming Language :: Python :: 2.7',
 			],
