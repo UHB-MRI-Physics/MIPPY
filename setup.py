2a7a20eeb517b03af0bef86caf35bb681131cef3
Rob Flintham <robert.flintham@uhb.nhs.uk>
2018-02-09 11:45:05 +0000

    BUILD 1.0.11 ::: Bugfix for pixel coordinate transformations

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 setup(	name='MIPPY',
-		version='1.0.10',
+		version='1.0.11',
 		description='Modular Image Processing in Python',
 		author='Robert Flintham',
 		author_email='robert.flintham@uhb.nhs.uk',
