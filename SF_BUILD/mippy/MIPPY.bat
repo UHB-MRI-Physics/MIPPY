34455d1791a1c5c947b2747cb76cd151b8d03e9e
Robert Flintham <rbf906@gmail.com>
2018-08-23 18:17:22 +0100

    Removed requirements from setup.py and added requirements.txt - also edited CI file to reflect this.

--- SF_BUILD/mippy/MIPPY.bat
+++
@@ -1,2 +0,0 @@
-python _mpy.py
-pause
--- SF_BUILD/mippy/setup/install.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-FOR /R deps %%F in (numpy*) do python -m pip install %%F & ^
-FOR /R deps %%F in (scipy*) do python -m pip install %%F & ^
-FOR /R current %%F in (MIPPY*) do python -m pip install %%F & ^
-
-pause
