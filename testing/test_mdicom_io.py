25cc9e36f560b5da504c7a727f10e56faa5371d4
Robert Flintham <rbf906@gmail.com>
2018-09-07 11:58:05 +0100

    Added test function and test data for save_dicom in mdicom.io

---
+++ testing/test_mdicom_io.py
@@ -0,0 +1,14 @@
+from mippy.mdicom.io import *
+import pydicom
+import numpy as np
+
+def test_save_dicom():
+	ds = pydicom.dcmread('test_data/ExampleDICOM/001.IMA')
+	px_data = ds.pixel_array.astype(np.float64)*1000
+	new_shape = (1,np.shape(px_data)[0],np.shape(px_data)[1])
+	save_dicom(px_data.reshape(new_shape),[ds],path='test_data/DCM-out',
+				fnames=['saved001.DCM'],rescale_slope='use_ref',
+				rescale_intercept='use_ref')
+	ds_loaded = pydicom.dcmread('test_data/DCM-out/saved001.DCM')
+	assert ds_loaded.pixel_array == 1000*ds.pixel_array
+	assert ds_loaded.SOPInstanceUID != ds.SOPInstanceUID
