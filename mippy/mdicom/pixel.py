Lots of bugfixing for enhanced DICOM images, removed multiprocessing for loading images into modules; Corrected str(uid) to uid.name in many places due to changes in pydicom; Added additional scaling factor finding for when it is buried in per-frame functional sequence in Philips images

mippy/mdicom/pixel.py
@@ -21,8 +21,22 @@ def get_px_array(ds,enhanced=False,instance=None,bitdepth=None):
                 ri = 0.
         try:
                 ss = float(ds[0x2005,0x100E].value)
+        except KeyError:
+                try:
+                        # Scaling buried per-frame in functional groups sequence
+                        ss = float(ds[0x5200,0x9230][instance-1][0x2005,0x140f][0][0x2005,0x100E].value)
+                except KeyError:
+                        ss = None
+                except:
+                        raise
         except:
-                ss = None
+                raise
+        #~ print("Scaling: RS {},RI {},SS {}".format(rs,ri,ss))
+        if ds.is_little_endian:
+                mode = 'littleendian'
+        else:
+                mode = 'bigendian'
+        #~ print(enhanced,mode)
         if compressed:
                 # Grab raw pixel array
                 #~ pixel_data_unpacked = binascii.unhexlify(ds.PixelData)
@@ -36,7 +50,7 @@ def get_px_array(ds,enhanced=False,instance=None,bitdepth=None):
                                 rows = int(ds.Rows)
                                 cols = int(ds.Columns)
                                 px_bytes = ds.PixelData[(instance-1)*(rows*cols*2):(instance)*(rows*cols*2)]
-                                px_float = px_bytes_to_array(px_bytes,rows,cols,rs=rs,ri=ri,ss=ss)
+                                px_float = px_bytes_to_array(px_bytes,rows,cols,rs=rs,ri=ri,ss=ss,mode=mode)
                         else:
                                 px_float = generate_px_float(ds.pixel_array.astype(np.float64),rs,ri,ss)
                 #~ else:
@@ -82,15 +96,18 @@ def px_bytes_to_array(byte_array,rows,cols,bitdepth=16,mode='littleendian',rs=1,
         if bitdepth==16:
                 if mode=='littleendian':
                         this_dtype = np.dtype('<u2')
-                else:
+                elif mode=='bigendian':
                         this_dtype = np.dtype('>u2')
+                else:
+                        print("Unsupported mode - use either littleendian or bigendian")
+                        return None
         elif bitdepth==8:
                 this_dtype = np.dytpe('u1')
         abytes = np.frombuffer(byte_array, dtype=this_dtype)
 #        print np.mean(abytes)
 #        print np.shape(abytes)
 #        print abytes
-        abytes = abytes.reshape((cols,rows))
+        abytes = abytes.reshape((cols,rows)).astype(np.float64)
         px_float = generate_px_float(abytes,rs,ri,ss)
 #        print np.mean(px_float)
         return px_float

mippy/viewing.py
@@ -1162,7 +1162,7 @@ class MIPPYImage():
             return
         bitdepth = int(ds.BitsStored)
         # DO NOT KNOW IF PIXEL ARRAY ALREADY HANDLES RS AND RI
-        pixels = ds.pixel_array.astype(np.float32)
+        pixels = ds.pixel_array.astype(np.float64)
         try:
             self.rs = float(ds[0x28, 0x1053].value)
         except:
@@ -1238,7 +1238,7 @@ class MIPPYImage():
             print(np.shape(pixel_array))
             pixel_array = np.mean(pixel_array, axis=0)
             print(np.shape(pixel_array))
-        self.px_float = pixel_array.astype(np.float32)
+        self.px_float = pixel_array.astype(np.float64)
         self.rangemax = np.amax(pixel_array)
         self.rangemin = np.amin(pixel_array)
         self.xscale = 1
@@ -1319,8 +1319,8 @@ class MIPPYImage():
         if self.level - self.rangemin < self.window / 2:
             self.window = 2 * (self.level - self.rangemin)
         windowed_px = np.clip(self.px_float, self.level - self.window / 2, self.level + self.window / 2 - 1).astype(
-            np.float32)
-        px_view = np.clip(((windowed_px - np.min(windowed_px)) / self.window * 256.), 0., 255.).astype(np.uint8)
+            np.float64)
+        px_view = np.clip(((windowed_px - np.min(windowed_px)) / self.window * 255.), 0., 255.).astype(np.uint8)
 
         self.image = Image.fromarray(px_view, mode='L')
 
@@ -1434,11 +1434,11 @@ class Image3D():
         ds_sorted = sorted(datasets, key=lambda x: x.ImagePositionPatient[sort_axis], reverse=True)
 
         # Create empty pixel array
-        px = np.zeros((nSlices, rows[0], cols[0])).astype(np.float32)
+        px = np.zeros((nSlices, rows[0], cols[0])).astype(np.float64)
 
         # Populate pixel array with slice data
         for i in range(len(ds_sorted)):
-            px[i] = ds_sorted[i].pixel_array().astype(np.float32)
+            px[i] = ds_sorted[i].pixel_array().astype(np.float64)
         # Store px as an attribute of the object
         self.px = px
 