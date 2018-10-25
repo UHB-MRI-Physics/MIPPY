cdf3d47683eace09ff4fc6f2eafb4562c943630a
Robert Flintham <rbf906@gmail.com>
2018-10-26 00:43:33 +0100

    Changed JPEG identifier to uid.name instead of str(uid); Debugging T1 shmolli fitting, currently includes prescribed test values of inv_times etc

--- mippy/mri/modelling.py
+++ mippy/mri/modelling.py
@@ -9,7 +9,7 @@ from mippy.viewing import MIPPYImage
 def magnitude_inversion_recovery(t,T1,M0):
     return abs(M0 * (1 - 2* np.exp(-t/T1)))
 
-def shmolli_t1_fit(images,inv_times,inv_scheme,rest_periods,rr_interval):
+def shmolli_t1_fit(images):
     """
     Given a 2D list of pydicom datasets sorted by inversion time, and provided
     with the inversion scheme (e.g. (5,1,1)), selectively use the inversion times
@@ -54,6 +54,23 @@ def shmolli_t1_fit(images,inv_times,inv_scheme,rest_periods,rr_interval):
                 # Same inversion as inv_times[j]
                 inv_numbers[i]=inv_numbers[j]
 
+    # TESTING only
+    TI_init = 100.
+    TI_inc = 80.
+    inv_scheme = (5,1,1)
+    inv_numbers = [1,1,1,1,1,2,3]
+    inv_times = [TI_init,
+                TI_init+rr_mean,
+                TI_init+2*rr_mean,
+                TI_init+3*rr_mean,
+                TI_init+4*rr_mean,
+                TI_init+TI_inc,
+                TI_init+2*TI_inc]
+
+    print(inv_times)
+    print(inv_numbers)
+    print(acq_times)
+
     # Get 3D pixel array
     px_list = []
     for i in range(len(images)):
@@ -64,41 +81,55 @@ def shmolli_t1_fit(images,inv_times,inv_scheme,rest_periods,rr_interval):
     dim = np.shape(im3d)
 
     # Fit for values from first inversion only
+    T_rec = (acq_times[inv_numbers.index(2)]-acq_times[inv_numbers.index(1)])*1000
+    print('Trec {}'.format(T_rec))
+    T_rec = rr_mean*6
+    print('Trec {}'.format(T_rec))
     inv_times = np.array(inv_times)
     inv_numbers = np.array(inv_numbers)
-    T_rec = (acq_times[inv_numbers.index(2)]-acq_times[inv_number.index(1)])*1000
 
     # Initialise T1 and M0 maps
-    t1_map = np.zeros(dim).astype(np.float64)
-    m0_map = np.zeros(dim).astype(np.float64)
-    rsquare_map = np.zeros(dim).astype(np.float64)
+    t1_map = np.zeros((dim[1],dim[2])).astype(np.float64)
+    m0_map = np.zeros((dim[1],dim[2])).astype(np.float64)
+    rsquare_map = np.zeros((dim[1],dim[2])).astype(np.float64)
+    fine_fit_map = np.zeros((dim[1],dim[2])).astype(np.float64)
+    fail_map = np.zeros((dim[1],dim[2])).astype(np.float64)
+
+    signal_theshold = 0.1*np.median(im3d[im3d>0])
 
     for y in range(dim[1]):
+        if y%10==0:
+            print('Fitting row {} of {}'.format(y+1,dim[1]))
         for x in range(dim[2]):
 
             init_T1 = 500
-            inti_M0 = 1000
+            init_M0 = 1000
             ydata = im3d[inv_numbers==1,y,x]
-            if 3*np.std(ydata)>np.mean(ydata):
+            if np.mean(ydata)<signal_theshold:
                 # Poor signal, ignore this pixel
+                fail_map[y,x] = 1.
                 continue
             xdata = inv_times[inv_numbers==1]
-            popt,pcov = curve_fit(shmolli_t1_fit,xdata,ydata,init_T1,init_M0)
+            popt,pcov = curve_fit(magnitude_inversion_recovery,xdata,ydata,[init_T1,init_M0])
             perr = np.sqrt(np.diag(pcov))
 
             # Check whether this is long or short T1
-            if popt[0]<5*T_rec:
+            if popt[0]<rr_mean:
                 # Short T1, re-fit with all echoes
+                fine_fit_map[y,x]=1.
                 ydata = im3d[:,y,x]
                 xdata = inv_times
-                popt,pcov = curve_fit(shmolli_t1_fit,xdata,ydata,init_T1,init_M0)
+                popt,pcov = curve_fit(magnitude_inversion_recovery,xdata,ydata,[init_T1,init_M0])
                 perr = np.sqrt(np.diag(pcov))
 
             if popt[0]<perr[0]*2:
                 # Less than 2 standard deviations, assume 0
+                # Fail code 1
+                fail_map[y,x] = 2.
                 continue
-            if popt<0:
+            if popt[0]<0:
                 # Negative T1 not feasible
+                fail_map[y,x] = 3.
                 continue
             # Calculate rsquare
             fit_data = magnitude_inversion_recovery(xdata,popt[0],popt[1])
@@ -107,7 +138,10 @@ def shmolli_t1_fit(images,inv_times,inv_scheme,rest_periods,rr_interval):
             rsquare = 1. - (SS_res/SS_tot)
 
             # Reject if rsquare<0.5
-            if rsqaure<0.5:
+            if rsquare<0.3:
+                # Fail code 3
+                fail_map[y,x] = 4.
+                rsquare_map[y,x] = rsquare
                 continue
 
 
@@ -115,9 +149,11 @@ def shmolli_t1_fit(images,inv_times,inv_scheme,rest_periods,rr_interval):
             # If all conditions above have failed, data is ok.  Add to maps.
             t1_map[y,x] = popt[0]
             m0_map[y,x] = popt[1]
-            rsquare_map[y,x] = rsqaure
+            rsquare_map[y,x] = rsquare
 
     # Dump to text files to open in imagej
     np.savetxt(r'C:\dump\t1.txt', t1_map)
     np.savetxt(r'C:\dump\m0.txt', m0_map)
     np.savetxt(r'C:\dump\rsquare.txt', rsquare_map)
+    np.savetxt(r'C:\dump\fine_fit.txt', fine_fit_map)
+    np.savetxt(r'C:\dump\fail.txt', fail_map)
